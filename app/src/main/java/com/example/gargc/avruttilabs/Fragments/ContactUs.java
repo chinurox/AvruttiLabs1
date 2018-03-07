bb02abfc7f623a79ca9150a6097b093b191ed78e
geekymehul <mehul.rawat40@gmail.com>
2018-03-07 18:09:40 +0530

    request and contact us

--- app/src/main/java/com/example/gargc/avruttilabs/Fragments/ContactUs.java
+++ app/src/main/java/com/example/gargc/avruttilabs/Fragments/ContactUs.java
@@ -1,11 +1,17 @@
 package com.example.gargc.avruttilabs.Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.gargc.avruttilabs.R;
 
@@ -14,6 +20,9 @@ import com.example.gargc.avruttilabs.R;
  */
 public class ContactUs extends Fragment {
 
+    private DatePicker datePicker;
+    private EditText edtId;
+    private Button btnSubmit;
 
     public ContactUs() {
         // Required empty public constructor
@@ -22,11 +31,48 @@ public class ContactUs extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_us, container, false);
+                             Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_contact_us, container, false);
+
+        datePicker = (DatePicker)view.findViewById(R.id.contact_date);
+        edtId = (EditText)view.findViewById(R.id.contact_order_id);
+        btnSubmit = (Button)view.findViewById(R.id.contact_submit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMail();
+            }
+        });
+
+        return view;
     }
 
+    private void sendMail()
+    {
+
+        String id = edtId.getText().toString();
+        int day = datePicker.getDayOfMonth();
+        int month = datePicker.getMonth() + 1;
+        int year = datePicker.getYear();
+        String date = day+"/"+month+"/"+year;
+
+        if(!TextUtils.isEmpty(id)&&!TextUtils.isEmpty(date))
+        {
+            Intent i = new Intent(Intent.ACTION_SEND);
+            i.setType("message/rfc822");
+            i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"mehul.rawat40@gmail.com"});
+            i.putExtra(Intent.EXTRA_SUBJECT, "subject of email");
+            i.putExtra(Intent.EXTRA_TEXT   , "order id : "+id+" date : "+date);
+            try {
+                startActivity(Intent.createChooser(i, "Send mail..."));
+            } catch (android.content.ActivityNotFoundException ex) {
+                Toast.makeText(getContext(), "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
 
 
 }
--- app/src/main/java/com/example/gargc/avruttilabs/Fragments/PCBRequest.java
+++ app/src/main/java/com/example/gargc/avruttilabs/Fragments/PCBRequest.java
@@ -1,13 +1,33 @@
 package com.example.gargc.avruttilabs.Fragments;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.gargc.avruttilabs.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,9 +35,15 @@ import com.example.gargc.avruttilabs.R;
 public class PCBRequest extends Fragment
 {
 
-    public PCBRequest() {
-        // Required empty public constructor
-    }
+    private EditText edtName,edtQty;
+    private Button btnSubmit,btnFile;
+    private Uri fileUri = null,path=null;
+    private DatabaseReference mDatabase;
+    private StorageReference mStorage;
+    private FirebaseAuth mAuth;
+    private String uid;
+
+    public PCBRequest() {}
 
 
     @Override
@@ -27,9 +53,113 @@ public class PCBRequest extends Fragment
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_pcbrequest, container, false);
 
+        edtName = (EditText)view.findViewById(R.id.pcb_name);
+        edtQty = (EditText)view.findViewById(R.id.pcb_qty);
+        btnFile = (Button)view.findViewById(R.id.pcb_upload_file);
+        btnSubmit = (Button)view.findViewById(R.id.pcb_submit);
 
+        mAuth = FirebaseAuth.getInstance();
+        uid = mAuth.getCurrentUser().getUid();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("PCBRequest").child(uid);
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+        btnFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("file/*");
+                startActivityForResult(intent,1);
+            }
+        });
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                sendEmail();
+            }
+        });
 
         return  view;
+    }
+
+    private void sendEmail()
+    {
+        final String name = edtName.getText().toString();
+        final String quantity = edtQty.getText().toString();
+
+        if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(quantity) && fileUri != null)
+        {
+            Intent emailIntent = new Intent(Intent.ACTION_SEND);
+            // set the type to 'email'
+            emailIntent.setType("vnd.android.cursor.dir/email");
+            String to[] = {"mehul.rawat40@gmail.com"};
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, to);
+            // the attachment
+            emailIntent.putExtra(Intent.EXTRA_STREAM, fileUri);
+            //the mail body
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "name :"+name+" quantity :"+quantity);
+            // the mail subject
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Subject");
+            try {
+                startActivity(Intent.createChooser(emailIntent, "Choose Gmail only..."));
+            } catch (android.content.ActivityNotFoundException ex) {
+                Toast.makeText(getContext(), "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void upload()
+    {
+        final String name = edtName.getText().toString();
+        final String quantity = edtQty.getText().toString();
+
+        if(!TextUtils.isEmpty(name)&&!TextUtils.isEmpty(quantity)&&fileUri!=null)
+        {
+            StorageReference filepath = mStorage.child("Requests").child(fileUri.getLastPathSegment());
+            filepath.putFile(fileUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot)
+                {
+
+                    @SuppressWarnings("VisibleForTests") String url = taskSnapshot.getDownloadUrl().toString();
+
+                    DatabaseReference userRequest = mDatabase.push();
+                    HashMap requestMap = new HashMap();
+                    requestMap.put("name",name);
+                    requestMap.put("quantity",quantity);
+                    requestMap.put("file",url);
+                    userRequest.setValue(requestMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()) {
+                                Toast.makeText(getContext(), "Request Sent", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                Toast.makeText(getContext(), "Could not be sent", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            });
+        }
+        else
+        {
+            Toast.makeText(getContext(), "Please enter all the fields", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==1 && resultCode==RESULT_OK)
+        {
+            fileUri = data.getData();
+        }
     }
 
 }
